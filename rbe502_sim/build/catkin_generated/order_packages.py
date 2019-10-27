a05311be90fe0c81e7cda7aa3ee613e421f2aaf7
Harry Saperstein <harrison.saperstein@gmail.com>
2019-10-27 16:06:13 -0400

    added robot sim dir

---
+++ rbe502_sim/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/devel;/ifs/home/hksaperstein/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/devel;/ifs/home/hksaperstein/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ rbe502_sim/build/rbe502_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/src/rbe502_sim
+# Build directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/build/rbe502_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
