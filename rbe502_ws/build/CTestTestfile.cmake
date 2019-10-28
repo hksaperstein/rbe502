0837226e8fcde8474411614ab2a079eed14e29b3
Harry Saperstein <harrison.saperstein@gmail.com>
2019-10-27 21:21:22 -0400

    fixed rbe502_sim package and all parts loaded into urdf. need proper transformation offsets and material properties

---
+++ rbe502_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_ws/src
+# Build directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rbe502_sim)
---
+++ rbe502_ws/build/rbe502_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_ws/src/rbe502_sim
+# Build directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_ws/build/rbe502_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
