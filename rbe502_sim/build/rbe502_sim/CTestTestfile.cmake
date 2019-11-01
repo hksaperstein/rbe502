aea93a67e72ad8c5e539d807fa34a57b8e6d5de2
Harrison Saperstein <hksaperstrein@wpi.edu>
2019-10-31 23:18:34 -0400

    removed outdated folder

--- rbe502_sim/build/rbe502_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/src/rbe502_sim
-# Build directory: /ifs/home/hksaperstein/Documents/git/rbe502/rbe502_sim/build/rbe502_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
