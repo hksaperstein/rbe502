0c5a0055c212a12f193b30feeb29d9fac55fe553
Andrew VanOsten <avanosten@wpi.edu>
2019-11-04 09:11:27 -0500

    MATLAB for 3D dynamic model and PD control

---
+++ MATLAB/system_PD_3D.m
@@ -0,0 +1,55 @@
+clc
+clear all
+close all
+
+%syms m m_w I I_w l r g theta(t) phi(t)
+global m m_w I I_w h r g q_d dq_d K_p K_d torque
+torque = [];
+g = 9.8;
+m = 1; m_w = 0.1; h = 0.25; r = 0.04;
+I = (1/3)*m*(2*h)^2; I_w = 2*(1/2)*m_w*r^2; 
+K_p = 100*eye(3);
+K_d = 10*eye(3);
+q_d = [0;1;1];
+dq_d = [0;0;0];
+q_0 = [pi/3;0;0];
+dq_0 = [0;0;0];
+X0 = [q_0; dq_0];
+[T,X] = ode45(@(t,x)ODE(t,x), [0 2], X0);
+figure;
+plot(T,X(:,1));
+figure;
+plot(T,X(:,2),T,X(:,3));
+
+function dx = ODE(t,x)
+    global torque
+    [M,C,G] = MCG(x);
+    dx(1:3,1) = x(4:6);
+    tau = PD_control(x,G);
+    %torque = [torque tau];
+    dx(4:6,1) = inv(M)*(tau - C - G);
+end
+
+% helper functions
+function [M,C,G] = MCG(x)
+    global m m_w I I_w h r g
+    theta  = x(1);
+    %phi_1  = x(2);
+    %phi_2  = x(3);
+    dtheta = x(4);
+    dphi_1 = x(5);
+    dphi_2 = x(6);
+    
+    M = [           m*h^2 + I,      (h*m*r*cos(theta))/2,       (h*m*r*cos(theta))/2;
+         (h*m*r*cos(theta))/2, I_w + (m*r^2)/4 + m_w*r^2,                  (m*r^2)/4;
+         (h*m*r*cos(theta))/2,                 (m*r^2)/4, I_w + (m*r^2)/4 + m_w*r^2];
+    C = [-(h*m*r*sin(theta)*dtheta*(dphi_1 + dphi_2))/2;
+                         -(h*m*r*sin(theta)*dtheta^2)/2;
+                         -(h*m*r*sin(theta)*dtheta^2)/2];
+    G = [-m*g*h*sin(theta); 0; 0];
+end
+
+function tau = PD_control(x, G)
+    global K_p K_d q_d dq_d
+    tau = K_p*(q_d - x(1:3)) + K_d*(dq_d - x(4:6)) + G;
+end
---
+++ MATLAB/system_model_3D.m
@@ -0,0 +1,44 @@
+%% 3D Dynamical Model of System
+
+clc
+clear all
+close all
+
+syms m m_w I I_w h r g phi_1(t)  phi_2(t) theta(t)
+
+v_1 = r*diff(phi_1(t),t);
+v_2 = r*diff(phi_2(t),t);
+v_0 = (v_1 + v_2)/2;
+v_m_sqr = v_0^2 + 2*v_0*h*diff(theta,t)*cos(theta) + (h*diff(theta,t))^2;
+
+K_m = 0.5*m*v_m_sqr + 0.5*I*diff(theta,t)^2;
+K_w = 0.5*I_w*(diff(phi_1,t)^2 + diff(phi_2,t)^2) + 0.5*m_w*(v_1^2 + v_2^2);
+P = m*g*h*cos(theta);
+
+L = K_m + K_w - P;
+
+tau(1,1) = diff(diffFunction(L, diff(theta(t),t)), t) - diffFunction(L, theta(t));
+tau(2,1) = diff(diffFunction(L, diff(phi_1(t),t)), t) - diffFunction(L, phi_1(t));
+tau(3,1) = diff(diffFunction(L, diff(phi_2(t),t)), t) - diffFunction(L, phi_2(t));
+
+M11 = simplify(tau(1) - subs(tau(1),diff(theta(t), t, t),0)) / diff(theta(t), t, t);
+M12 = simplify(tau(1) - subs(tau(1),diff(phi_1(t), t, t),0)) / diff(phi_1(t), t, t);
+M13 = simplify(tau(1) - subs(tau(1),diff(phi_2(t), t, t),0)) / diff(phi_2(t), t, t);
+M21 = simplify(tau(2) - subs(tau(2),diff(theta(t), t, t),0)) / diff(theta(t), t, t);
+M22 = simplify(tau(2) - subs(tau(2),diff(phi_1(t), t, t),0)) / diff(phi_1(t), t, t);
+M23 = simplify(tau(2) - subs(tau(2),diff(phi_2(t), t, t),0)) / diff(phi_2(t), t, t);
+M31 = simplify(tau(3) - subs(tau(3),diff(theta(t), t, t),0)) / diff(theta(t), t, t);
+M32 = simplify(tau(3) - subs(tau(3),diff(phi_1(t), t, t),0)) / diff(phi_1(t), t, t);
+M33 = simplify(tau(3) - subs(tau(3),diff(phi_2(t), t, t),0)) / diff(phi_2(t), t, t);
+M = [M11 M12 M13; M21 M22 M23; M31 M32 M33];
+
+G = subs(tau, {diff(theta(t), t, t), diff(phi_1(t), t, t), diff(phi_2(t), t, t), diff(theta(t), t), diff(phi_1(t), t), diff(phi_2(t), t)}, {0,0,0,0,0,0});
+
+C = simplify(tau - M*{diff(theta,t,t); diff(phi_1,t,t); diff(phi_2,t,t)} - G);
+
+function new_f=diffFunction(f,x)
+    syms temp
+    new_f = subs(f,x,temp);
+    new_f = diff(new_f,temp);
+    new_f = subs(new_f,temp,x);
+end
