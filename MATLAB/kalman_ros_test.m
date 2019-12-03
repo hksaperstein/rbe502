30f6f001e3f34891e9ffbe5cc83b487bc55893b8
Andrew VanOsten <avanosten@wpi.edu>
2019-12-02 22:43:35 -0500

    Hopefully somewhat finished

--- MATLAB/kalman_ros_test.m
+++ MATLAB/kalman_ros_test.m
@@ -1,8 +1,66 @@
-% state: [theta dtheta]
-A = [1 dt;
-     0  1];
-Q = [0.05; 0.05]; % process noise
+rosinit;
+IMUSub = rossubscriber('/imu');
+VelSub = rossubscriber('/joint_states');
 
-x_pred = A*x_old; % State prediction, no input
-P_pred = A*P_old*A.' + Q; % Covariance prediction
-y_tilde = 
+data = [];
+time = [];
+tic
+t = toc;
+while(t < 15)
+    IMUdata = receive(IMUSub);
+    %qx = IMUdata.Orientation.X;
+    qy = IMUdata.Orientation.Y;
+    %qz = IMUdata.Orientation.Z;
+    qw = IMUdata.Orientation.W;
+    theta = 2*atan2(qy,qw)*180/pi;
+    
+    veldata = receive(VelSub);
+    data = [data veldata];
+    
+    t = toc;
+end
+rosshutdown;
+
+% x = []; y = []; z = [];
+% theta = []; theta2 = [];
+% qx = []; qy = []; qz = []; qw = [];
+% for i = 1:length(data)
+%     x(i) = data(i).LinearAcceleration.X;
+%     y(i) = data(i).LinearAcceleration.Y;
+%     z(i) = data(i).LinearAcceleration.Z;
+%     theta(i) = atan2(x(i),norm([x(i), z(i)]));
+%     qx(i) = data(i).Orientation.X;
+%     qy(i) = data(i).Orientation.Y;
+%     qz(i) = data(i).Orientation.Z;
+%     qw(i) = data(i).Orientation.W;
+%     theta2(i) = 2*atan2(qy(i),qw(i))*180/pi;
+% end
+% subplot(4,1,1)
+% plot(time,x,'r-')
+% subplot(4,1,2)
+% plot(time,y,'g-')
+% subplot(4,1,3)
+% plot(time,z,'b-')
+% subplot(4,1,4)
+% plot(time,theta,'r-')
+% 
+% figure;
+% subplot(5,1,1)
+% plot(time,qx,'r-')
+% subplot(5,1,2)
+% plot(time,qy,'g-')
+% subplot(5,1,3)
+% plot(time,qz,'b-')
+% subplot(5,1,4)
+% plot(time,qw,'r-')
+% subplot(5,1,5)
+% plot(time,theta2)
+
+% state: [theta dtheta]
+% A = [1 dt;
+%      0  1];
+% Q = [0.05; 0.05]; % process noise
+% 
+% x_pred = A*x_old; % State prediction, no input
+% P_pred = A*P_old*A.' + Q; % Covariance prediction
+% y_tilde = 
--- MATLAB/system_ODE.m
+++ MATLAB/system_ODE.m
@@ -7,70 +7,112 @@ g = 9.8;
 m = 1.51518; m_w = 0.053337; h = 0.057794; r = 0.04445;
 I = 0.02054; I_w = 0.00004896;
 
-% PD values
-K_p = 100*eye(3);
-K_d = 10*eye(3);
+kp = 2;
+kd = 0;
+K_p = [ kp 0;
+       -kp 0];
+K_d = [ kd 0;
+       -kd 0];
 
 % desired state
-q_d = [0;0;0];
-dq_d = [0;0;0];
+q_d = [0;0];
+dq_d = [0;0];
 
+global Veldata IMUdata
+Veldata = 0;
+IMUdata = 0;
 % ROS Setup
 rosinit;
 vels = rospublisher('/cmd_vel','geometry_msgs/Twist');
-wheelVel = rosmessage('geometry_msgs/Twist');
-IMUSub = rossubscriber('/imu');
-VelSub = rossubscriber('/joint_states'); %this is currently broken till harry adds the publisher
-
-
+IMUSub = rossubscriber('/imu', @updateIMU);
+VelSub = rossubscriber('/joint_states',@updateVel);
+while(IMUdata == 0)
+    pause(0.1);
+end
+while(Veldata == 0)
+    pause(0.1);
+end
+disp(IMUdata)
+qy = IMUdata.Orientation.Y;
+qw = IMUdata.Orientation.W;
+old_theta = 2*atan2(qy,qw)*180/pi;
+tic;
+old_t = toc;
+old_phi_1 = 0;
+old_phi_2 = 0;
+i = 0;
 while(1)
+    t = toc;
+    dt = t - old_t;
+    old_t = t;
     % Get new state data from ROS
-    Veldata = receive(VelSub); % 10 is the timeout in s, yes this is blocking...
-    IMUdata = receive(IMUSub);
+    %Veldata = receive(VelSub); % 10 is the timeout in s, yes this is blocking...
+    %IMUdata = receive(IMUSub);
+    qy = IMUdata.Orientation.Y;
+    qw = IMUdata.Orientation.W;
+    theta = 2*atan2(qy,qw)*180/pi;
+    
+    dphi_1 = (Veldata.Position(2) - old_phi_1)/dt;
+    dphi_2 = (Veldata.Position(1) - old_phi_2)/dt;
+    dphi = (dphi_1 + dphi_2)/2;
+    old_phi_1 = Veldata.Position(2);
+    old_phi_2 = Veldata.Position(1);
     
     %Build up state vector
-    x = zeros(6,1);
-    % x(1) = ...
-    % somehow get an angle out of this... Kalman?
+    x = zeros(4,1);
+    x(1) = theta;
+    x(3) = (theta - old_theta)/dt;
+    x(4) = dphi;
     
     % Call the ODE function
     % this applies the state-space model
     dx = ODE(x);
     
-    % use wheel speeds from dx to calculate linear x and angular z
+    new_x = dx*dt + x;
     
-    %wheelVel.Linear.X = 0.1;
+    % use wheel speeds from dx to calculate linear x and angular z
+    wheelVel = rosmessage('geometry_msgs/Twist');
+    wheelVel.Linear.X = r*new_x(4);
+    send(vels,wheelVel);
 end
 
 % call this function with the state and it will
 % give back the derivative of state (which includes wheel speeds)
 function dx = ODE(x)
+    %global torque
     [M,C,G] = MCG(x);
-    dx(1:3,1) = x(4:6);
+    dx(1:2,1) = x(3:4);
     tau = PD_control(x,G);
-    dx(4:6,1) = M\(tau - C - G);
+    %torque = [torque [t; tau]];
+    dx(3:4,1) = M\(tau - C - G);
 end
 
 % helper functions
 function [M,C,G] = MCG(x)
     global m m_w I I_w h r g
     theta  = x(1);
-    %phi_1  = x(2); % wheel angles aren't used by the model
-    %phi_2  = x(3);
-    dtheta = x(4);
-    dphi_1 = x(5);
-    dphi_2 = x(6);
-    
-    M = [           m*h^2 + I,      (h*m*r*cos(theta))/2,       (h*m*r*cos(theta))/2;
-         (h*m*r*cos(theta))/2, I_w + (m*r^2)/4 + m_w*r^2,                  (m*r^2)/4;
-         (h*m*r*cos(theta))/2,                 (m*r^2)/4, I_w + (m*r^2)/4 + m_w*r^2];
-    C = [-(h*m*r*sin(theta)*dtheta*(dphi_1 + dphi_2))/2;
-                         -(h*m*r*sin(theta)*dtheta^2)/2;
-                         -(h*m*r*sin(theta)*dtheta^2)/2];
-    G = [-m*g*h*sin(theta); 0; 0];
+    % phi    = x(2);
+    dtheta = x(3);
+    dphi   = x(4);
+    M = [          m*h^2 + I,       h*m*r*cos(theta);
+         h*m*r*cos(theta), 2*I_w + m*r^2 + 2*m_w*r^2];
+    C = [-h*m*r*sin(theta)*dphi*dtheta;
+            -h*m*r*sin(theta)*dtheta^2];
+    G = [-g*h*m*sin(theta);
+                         0];
 end
 
 function tau = PD_control(x, G)
     global K_p K_d q_d dq_d
-    tau = K_p*(q_d - x(1:3)) + K_d*(dq_d - x(4:6)) + G;
+    tau = K_p*(q_d - x(1:2)) + K_d*(dq_d - x(3:4)) + G;
+end
+
+function updateVel(~, Veldata_in, ~)
+    global Veldata
+    Veldata = Veldata_in;
+end
+
+function updateIMU(~, IMUdata_in, ~)
+    global IMUdata
+    IMUdata = IMUdata_in;
 end
